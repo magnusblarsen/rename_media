8e1152d085d7b4e3a212f8cde69837497411af8f
brml <brml@itu.dk>
2024-10-18 10:40:28 +0200

    Use date created property instead of date modified

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+Uses `exiftool` to rename pictures/videos to the date they were created.
+
 Specify folders to look inside. 
 Looks inside each folder and subfolders of that folder.
--- rename_pictures.sh
+++ rename_pictures.sh
@@ -3,6 +3,7 @@
 # TODO: flag: keep file names
 
 file_extensions_regex="jpg|jpeg|png|heic|mp4|mov"
+keep_file_names=false
 
 rename_file() {
 	local file_info="$1" # Ex: 2007-05-05Billeroverførtmajmd07 001.jpg
@@ -23,6 +24,19 @@ rename_file() {
 	mv "$file_path" "$new_destination"
 }
 
+find_file_info() {
+	local file_name="$1"
+
+	creation_date=$(exiftool -CreateDate -d "%Y-%m-%d %H:%M:%S" "$file_name")
+
+	if [[ -n "$creation_date" ]]; then
+		new_name="${creation_date} $1"
+	fi
+
+	echo "$new_name"
+}
+
+export -f find_file_info
 
 home_folder=$(pwd)
 
@@ -32,8 +46,8 @@ for folder in "$@"; do
 
 	sorted_files=$(find . -maxdepth 2 -type f | # Sometimes pictures are in subfolders
 		grep -Ei ".*(${file_extensions_regex})$" | 
-		xargs -d '\n' stat -c "%y %n" | sort | # Ex: 2007-05-05 18:22:15.000000000 +0200 ./Billeroverførtmajmd07 020.jpg
-		sed -E "s/^([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{9} (\+|\-)[0-9]{4}) (.*)$/\1\4/")
+		xargs -I {} bash -c 'find_file_info "$@"' _ {} | sort | 
+		sed -E "s/^Create Date *: ([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) (.*)$/\1\3/") # Create Date    : 2006-07-14 21:58:38 ./Billeroverførtmajmd07 020.jpg
 
 	IFS=$'\n'
 	for file in $sorted_files; do
